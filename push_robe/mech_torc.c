edbc4432acbd1b34194fd6e3701b9974e210f4dd
Matteo Camilli <mcamilli@e3r11p5.42roma.it>
2024-01-13 16:19:03 +0100

    ok

--- push_robe/mech_torc.c
+++ push_robe/mech_torc.c
@@ -6,7 +6,7 @@
 /*   By: mcamilli <mcamilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:30:18 by mcamilli          #+#    #+#             */
-/*   Updated: 2024/01/12 13:19:12 by mcamilli         ###   ########.fr       */
+/*   Updated: 2024/01/13 16:06:12 by mcamilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,4 +75,154 @@ int find_posb(int x, t_stack *stack) //trova la pos che devo far diventare 0
 	return (-1);
 }
 
+void setall(int posb, int posa, t_stack *stack)
+{
+	stack->defa = posa;
+	stack->defb = posb;
+}
 
+void find_move(int posb, int posa, t_stack *stack)
+{
+	if (posb < posa)
+	{
+		if (stack->mosse > posa + 1) //upup
+		{
+			stack->mosse = posa + 1;
+			stack->move = 1;
+			setall(posb, posa, stack);
+		}
+		if (stack->mosse > (stack->last_a - posa + 1) +
+		((stack->last_b - posb + 1) - (stack->last_a - posa + 1)) + 1) //down down
+		{
+			stack->mosse = (stack->last_b - posb + 1) +
+			((stack->last_a - posa + 1) - (stack->last_b - posb + 1)) + 1;
+			stack->move = 2;
+			setall(posb, posa, stack);
+		}
+	}
+	if (posa < posb)
+	{
+		if (stack->mosse > posb + 1) //upup
+		{
+			stack->mosse = posb + 1;
+			stack->move = 1;
+			setall(posb, posa, stack);
+		}
+		if (stack->mosse > (stack->last_b - posb + 1) +
+		((stack->last_a - posa + 1) - (stack->last_b - posb + 1)) + 1) //down down
+		{
+			stack->mosse = (stack->last_b - posb + 1) +
+			((stack->last_a - posa + 1) - (stack->last_b - posb + 1)) + 1;
+			stack->move = 2;
+			setall(posb, posa, stack);
+		}
+	}
+	if (stack->mosse > (posb + 1 + (stack->last_a - posa) + 1)) // up down
+	{
+		stack->mosse = posb + 1 + (stack->last_a - posa) + 1;
+		stack->move = 3;
+		setall(posb, posa, stack);
+	}
+	if (stack->mosse > (stack->last_b - posb + 1 + posa + 1)) //down up
+	{
+		stack->mosse = stack->last_b - posb + 1 + posa + 1;
+		stack->move = 4;
+		setall(posb, posa, stack);
+	}
+}
+
+
+void movesinuse(t_stack *stack)
+{
+	if (stack->move == 1 && stack->defb << stack->defa)
+	{
+
+	}
+	if (stack->move == 1 && stack->defb >> stack->defa)
+	{
+
+	}
+	if (stack->move == 2 && stack->defb << stack->defa)
+	{
+
+	}
+	if (stack->move == 2 && stack->defb << stack->defa)
+	{
+
+	}
+	if (stack->move == 3)
+	{
+
+	}
+	if (stack->move == 4)
+	{
+
+	}
+}
+
+void ft_upupamagb(t_stack *stack)
+{
+	int i;
+
+	i = stack->defb;
+	while (i--)
+		ft_rr(stack);
+	i = stack->defa - stack->defb;
+	while (i--)
+	 ft_ra(stack);
+	ft_pa(stack)
+}
+
+void ft_upupaminb(t_stack *stack)
+{
+	int i;
+
+	i = stack->defa;
+	while (i--)
+		ft_rr(stack);
+	i = stack->defb - stack->defa;
+	while (i--)
+	 ft_rb(stack);
+	ft_pa(stack);
+}
+
+void ft_downdownamagb(t_stack *stack)
+{
+	int i;
+
+	i = stack->last_a - stack->defa + 1;
+	while (i--)
+		ft_rrr(stack);
+	i = (stack->last_b - stack->defb + 1) - (stack->last_a - stack->defa + 1);
+	while (i--)
+		ft_rrb(stack);
+	ft_pa(stack);
+
+}
+
+void ft_downdownaminb(t_stack *stack)
+{
+	int i;
+
+	i = stack->last_b - stack->defb + 1;
+	while (i--)
+		ft_rrr(stack);
+	i = (stack->last_a - stack->defa + 1) - (stack->last_b - stack->defb + 1);
+	while (i--)
+		ft_rra(stack);
+	ft_pa(stack);
+
+}
+
+void ft_updown(t_stack *stack)
+{
+	int i;
+
+	i = stack->defb;
+	while (i--)
+		ft_rb(stack);
+	i = stack->last_a - stack->defa + 1;
+	while (i--)
+		ft_rb(stack);
+
+void ft_downup(t_stack *stack)
---
+++ push_robe/movesinuse.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movesinuse.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcamilli <mcamilli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 14:30:28 by mcamilli          #+#    #+#             */
+/*   Updated: 2024/01/13 14:35:13 by mcamilli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+
+void movesinuse(t_stack *stack)
+{
+	if (stack->move == 1 && stack->defb << stack->defa)
+	{
+
+	}
+	if (stack->move == 1 && stack->defb >> stack->defa)
+	{
+
+	}
+	if (stack->move == 2 && stack->defb << stack->defa)
+	{
+
+	}
+	if (stack->move == 2 && stack->defb << stack->defa)
+	{
+
+	}
+	if (stack->move == 3)
+	{
+
+	}
+	if (stack->move == 4)
+	{
+
+	}
+}
+void ft_upupamagb(t_stack *stack)
+{
+	
+}
+
+void ft_upupaminb(t_stack *stack)
+
+void ft_downdownamagb(t_stack *stack)
+
+void ft_downdownaminb(t_stack *stack)
+
+void ft_updown(t_stack *stack)
+
+void ft_downup(t_stack *stack)
